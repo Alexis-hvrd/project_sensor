97697fa4353783d562a03d779f45e67acb197ecf
Alexis-hvrd <150938044+Alexis-hvrd@users.noreply.github.com>
2023-11-28 17:10:30 +0100

    Delete project_sensor/sans_expl directory

--- project_sensor/sans_expl/site/commun/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Data Graphics</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-   <link rel="stylesheet" type="text/css" media="screen" href="monsite.css"/>
-   
-</head>
